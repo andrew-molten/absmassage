dda51adca241f55d967d1044d2c2a6e9494be55c
Andrew Molten <andrewmolten23@gmail.com>
2024-08-22 11:49:56 +1200

    change font, style links

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -4,52 +4,54 @@ import '../styles/NavBar.scss'
 
 function NavBar() {
   return (
-    <div className="flex">
+    <div className="flex justify-between">
       <img
         src={absmlogo}
         alt="Andrew Bolton Sports Massage logo"
         className="header-logo"
       />
-      <NavLink
-        to={'/'}
-        className={({ isActive }) =>
-          isActive ? 'nav-link active-link' : 'nav-link'
-        }
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to={'/massage-&-prices'}
-        className={({ isActive }) =>
-          isActive ? 'nav-link active-link' : 'nav-link'
-        }
-      >
-        Massage & Prices
-      </NavLink>
-      <NavLink
-        to={'/about'}
-        className={({ isActive }) =>
-          isActive ? 'nav-link active-link' : 'nav-link'
-        }
-      >
-        About
-      </NavLink>
-      <NavLink
-        to={'/faq'}
-        className={({ isActive }) =>
-          isActive ? 'nav-link active-link' : 'nav-link'
-        }
-      >
-        FAQ
-      </NavLink>
-      <NavLink
-        to={'/contact'}
-        className={({ isActive }) =>
-          isActive ? 'nav-link active-link' : 'nav-link'
-        }
-      >
-        Contact
-      </NavLink>
+      <div className="flex justify-between self-end">
+        <NavLink
+          to={'/'}
+          className={({ isActive }) =>
+            isActive ? 'nav-link active-link' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to={'/massage-&-prices'}
+          className={({ isActive }) =>
+            isActive ? 'nav-link active-link' : 'nav-link'
+          }
+        >
+          Massage & Prices
+        </NavLink>
+        <NavLink
+          to={'/about'}
+          className={({ isActive }) =>
+            isActive ? 'nav-link active-link' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to={'/faq'}
+          className={({ isActive }) =>
+            isActive ? 'nav-link active-link' : 'nav-link'
+          }
+        >
+          FAQ
+        </NavLink>
+        <NavLink
+          to={'/contact'}
+          className={({ isActive }) =>
+            isActive ? 'nav-link active-link' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </div>
     </div>
   )
 }
