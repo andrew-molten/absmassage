a98fa7176e60ab2dc0cc5a1a2acedbcd322d860d
Andrew Molten <andrewmolten23@gmail.com>
2024-08-30 16:59:11 +1200

    improve written content massage & prices

--- client/pages/MassagePrices.tsx
+++ client/pages/MassagePrices.tsx
@@ -2,6 +2,7 @@ import leafyMassageSpace from '../../images/slider/massage-space-leafy.webp'
 import andrewMassaging from '../../images/slider/andrew-massaging.webp'
 import neckMassage from '../../images/slider/Andrew-Bolton-Sports-Massage(sm).webp'
 import malasana from '../../images/andrew/Malasana.webp'
+// import crescentLunge from '../../images/andrew/Crescent-lunge.webp'
 
 function MassagePrices() {
   return (
@@ -49,26 +50,16 @@ function MassagePrices() {
           <div className="column">
             <h2>Deep Tissue Massage</h2>
             <p>
-              Deep tissue massage centers on realigning the deeper layers of
-              connective and muscle tissue. It aims to release the chronic
-              patterns of tension in the body through slow strokes and deep
-              finger pressure on the tense areas, either following or going
-              across the fiber’s of the muscles, tendons and fascia. It is
-              particularly helpful for continually tight and contracted areas
-              such as stiff necks, low back tightness, and sore shoulders.
+              Deep tissue massage uses firm pressure with slow strokes to work
+              deeply into tense areas and those around them releasing chronic
+              tension. Especially effective for persistent tightness in your
+              body.
             </p>
             <p>
-              Deep tissue massage is much more than just a “hard massage.” In
-              contrast to only relaxing muscles, the specific lengthening of
-              fascia, muscles and tendons offers many benefits such as increased
-              joint movement, quicker injury recovery, better posture, as well
-              as feelings of well-being.
-            </p>
-            <p>
-              Deep tissue massage is often used to treat chronic pain, limited
-              mobility, recovery from injuries (e.g. whiplash, falls, and sports
-              injury), repetitive strain injury (carpal tunnel syndrome),
-              postural problems, osteoarthritis pain, muscle tension or spasm.
+              Not just a “hard massage”, the specific lengthening & the deep
+              work into muscles, fascia & tendons throughout the body improve
+              joint movement, tension, posture & overall well-being, or to aid
+              in a quick recovery from injury.
             </p>
           </div>
           <div className="column image-column">
@@ -90,25 +81,20 @@ function MassagePrices() {
           <div className="column">
             <h2>Sports Massage</h2>
             <p>
-              Sports massage can be beneficial for athletes, active people or
-              anybody with tension. Often has a lot of overlap with deep tissue
-              massage, depending on what you need, while also incorporating more
-              techniques such as kneading, compressions & cirular friction to
-              help break down adhesions.
+              Sports massage can be more dynamic, often borrowing deep tissue
+              and other techniques such as kneading, compressions & circular
+              friction depending on what you need.
             </p>
 
             <p>
-              Regular sports massage can help increase flexibility, realign
-              muscle fibres, and restore muscle length, shape and strength. It
-              can also be used to aid training to reduce fatigue, injury and
-              delayed onset muscle soreness, allowing you to perform at your
-              best.
+              It’s beneficial for athletes, active people & anybody with
+              tension. Used to overcome injury, help with training, increase
+              flexibility, relieve tension and reduce fatigue, injury & delayed
+              onset muscle soreness.
             </p>
             <p>
-              I tailor each massage to the individual, so whether you like your
-              massage strong and deep or soft and gentle I am here to serve you.
-              I use different techniques for different bodies, as every body is
-              different and reacts differently.
+              We might target specific problem areas or do a general full-body
+              massage. Either way, I tailor each massage to your needs.
             </p>
           </div>
         </div>
@@ -116,22 +102,17 @@ function MassagePrices() {
           <div className="column">
             <h2>Relaxing massage</h2>
             <p>
-              This is normally a whole body therapeutic massage designed to
-              relax your muscles and joints, stimulate blood flow and calm your
-              nervous system. You can of course still request specific area that
-              you would rather focus more attention on.
+              A whole-body therapeutic massage using long rhythmic strokes to
+              relax your muscles & joints, stimulate blood flow and calm your
+              nervous system.
             </p>
             <p>
-              Using long rhythmic strokes with pressure that is gentle, moderate
-              or deep according to your preference. Unwind to peaceful music and
-              relaxing scents.
+              I use pressure according to your preference, with relaxing music
+              playing. Your choice to have a chat or go quiet.
             </p>
             <p>
-              I will pay attention to your body as a whole, and will still work
-              on any areas that are holding more tension, the difference being
-              that this will be much more gentle than a deep tissue massage, so
-              you can relax deeply. If the pressure ever gets too much, just let
-              me know so I can ease off.
+              You can request areas you want to focus on and I will work to
+              relax any tension away.
             </p>
           </div>
           <div className="column image-column">
@@ -148,54 +129,26 @@ function MassagePrices() {
           </div>
           <div className="column">
             <h2>Private yoga session</h2>
+
+            <p>{`I've been practising yoga since 2010 & teaching since 2016.`}</p>
             <p>
-              Are you trying to find more movement and/or mobility in your life?
-            </p>
-            <p>I've been practicing yoga since 2010, & teaching since 2016.</p>
-            <p>
-              Over that time I have learnt from many other teachers and movement
-              experts. I've worked with the super flexible and inflexible! With
-              people working on arm balances, and beginners just working on the
-              basics.
+              {`I keep learning from other teachers and movement experts. I've worked with the super flexible and inflexible! With people doing arm balances & beginners mastering the basics.`}
             </p>
             <p>
-              As you may have noticed by now I am passionate about functional
-              movement. This flows into my yoga style too. The first thing we'll
-              do when you come to practice yoga with me is figure out what you
-              can do easily, and what you find more challenging, I want to know
-              what your lifestyle is like, what' your movement practice is
-              like?What are your goals? Then we will combine breathing
-              techniques and yoga poses to help you get stronger and more
-              mobile.
+              {`Depending on what your needs are the practice can be slow and methodical, flowy, strong & powerful, meditative, fast-paced, relaxing, or a combination of styles. `}
             </p>
             <p>
-              Depending on what your needs and comfort levels are, the practice
-              may be slow and methodical, flowy, loosy goosy, strong & powerful,
-              meditative, fast-paced, zen & relaxing, or a combination of all
-              these styles. Within yoga there are some general alignment rules
-              to keep you safe from injury - I pay close attention to these
-              rules, buut sometimes rules are made to be broken, once you have a
-              good foundational understanding of the rules in your body then we
-              can play more and more with how to break the rules in healthy
-              ways!
+              Learn how to align your body, moving with the breath in a safe way
+              to build strength & improve mobility.
             </p>
             <p>
-              The benefits of private yoga are that you have the freedom to ask
-              questions, there's nobody else in the room to worry about, you
-              have more agency over what kind of practice you would like,
-              together we can create a practice that moves you out of your
-              normal patterns, a practice that pushes you and makes you sweat,
-              or a practice that brings you that much needed relaxation.
-            </p>
-            <p>
-              Whether you're practicing with me through winter or in the summer
-              I will get this room to the perfect temperature for you, with my
-              dual heaters for warmth, and windows for fresh air!
+              This is a great space to do yoga in, with the freedom to focus on
+              your yoga goals, get in touch if you have any questions.
             </p>
           </div>
         </div>
         <div className="row column-reverse">
-          <div className="column">
+          {/* <div className="column">
             <h2>Group yoga classes</h2>
             <p>
               {`I have not been teaching many group yoga classes, as I can’t quite
@@ -215,14 +168,14 @@ function MassagePrices() {
               <a href="Contact.html">get in touch</a> with an outline of what
               you have in mind and we can work together to make it happen.
             </p>
-          </div>
-          <div className="column image-column">
+          </div> */}
+          {/* <div className="column image-column">
             <img
               className="col-img"
-              src="Website photos\Andrew\Cresent lunge.jpg"
-              alt="Strong Sports Massage"
+              src={crescentLunge}
+              alt="Yoga pose - cresent lunge"
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
