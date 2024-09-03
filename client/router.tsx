10b5e768a563f4b2a941c070c25f679f0284ab82
Andrew Molten <andrewmolten23@gmail.com>
2024-09-03 13:48:09 +1200

    add privacy-policy

--- client/router.tsx
+++ client/router.tsx
@@ -10,7 +10,8 @@ import MassagePrices from './pages/MassagePrices.tsx'
 import About from './pages/About.tsx'
 import FAQ from './pages/FAQ.tsx'
 import Contact from './pages/Contact.tsx'
-import RefundCancellations from './pages/Refund-Cancellations.tsx'
+import RefundCancellations from './pages/RefundCancellations.tsx'
+import PrivacyPolicy from './pages/PrivacyPolicy.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route path="/faq" element={<FAQ />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/refund-cancellations" element={<RefundCancellations />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Route>,
   ),
 )
