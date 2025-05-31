2c4dc7068d73c03049f94e8d34a982a1281c40ae
Andrew Molten <andrewmolten23@gmail.com>
2025-05-31 20:18:15 +1200

    improve slider photos

--- images/slider/sliderImages.js
+++ images/slider/sliderImages.js
@@ -5,13 +5,15 @@ import massageHands from './massage-hands.webp'
 import massageStudio from './massage-studio.webp'
 import massageSpace from './massage-space.webp'
 import leafy from './leafy.webp'
+import andrew from './andrew.webp'
 
 export const sliderImages = [
+  { image: andrew, alt: 'andrew' },
   { image: massageSpace, alt: 'leafy massage space' },
   { image: andrewMassaging, alt: 'andrew masaging' },
   { image: massageStudio, alt: 'warm massage studio' },
   { image: leafy, alt: 'massage consultation space' },
   { image: massageHands, alt: 'hands massaging' },
-  { image: absm, alt: 'relaxing neck massage' },
   { image: indiaAndrew, alt: 'Andrew in India' },
+  { image: absm, alt: 'relaxing neck massage' },
 ]
