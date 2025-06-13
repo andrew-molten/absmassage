cbc79104ed76236e25cdc9b9fc429caac80d87b9
Andrew Molten <andrewmolten23@gmail.com>
2025-06-13 14:10:12 +1200

    remove old useeffect from GoogleReviewsWidget

--- src/components/GoogleReviewsWidget.tsx
+++ src/components/GoogleReviewsWidget.tsx
@@ -359,22 +359,6 @@ const GoogleReviewsWidget: FC<GoogleReviewsWidgetProps> = ({
     }
   }, [])
 
-  // useEffect(() => {
-  //   if (reviews.length > 0) {
-  //     const newArray = [...reviews]
-  //     for (let i = newArray.length - 1; i > 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1))
-  //       ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
-  //     }
-  //     setShuffledReviews(newArray)
-  //     setTotalReviewsCount(newArray.length)
-  //   } else {
-  //     setShuffledReviews([])
-  //     setTotalReviewsCount(0)
-  //   }
-  //   setCurrentIndex(0)
-  // }, [reviews])
-
   useEffect(() => {
     if (reviews.length > 0) {
       // Helper function to convert star rating strings to numbers for sorting
